fffee414790573ebf96c184bdaad511098ade0a6
Omer <momeriqbal2@gmail.com>
2013-10-05 19:14:11 +0800

    added helpful comments

--- adder32.vhd
+++ adder32.vhd
@@ -40,16 +40,24 @@ signal complementedAdd : std_logic_vector(31 downto 0);
 signal interResult :  std_logic_vector(31 downto 0);
 signal maxNegative : std_logic_vector(31 downto 0) := (31 => '1', others => '0');
 begin
+-- interResult buffers the result
+-- When both numbers are positive, signed addition is pretty much quivalent to unsigned addition.
+-- We use unsigned as we want to preserve the last bit in case of overflows
 interResult <= std_logic_vector( unsigned(operand1) + unsigned(operand2)) when (operand1(31) = '0' and operand2(31) = '0') else
+-- When both numbers are postive or negative we perform standard signed addition to get the result
 			      std_logic_vector( signed(operand1) + signed(operand2));
+-- Calculating overflow is tricky when both numbers are negative. So instead convert the negative numbers to positive using 2's complement
+-- We find the overflow then using the same idea as in unsigned numbers
+-- When performing 2's complement need to watch out for the maximum possible negative number
 complementedAdd <= std_logic_vector(unsigned(not(operand1))+1 + unsigned(not(operand2)) + 1) when (operand1 /= maxNegative and operand2 /= maxNegative) else
                    maxNegative;
+-- overflow is the MSB of interResult when both numbers are positive
+-- Its the MSB of complementedAdd when both numbers are negative
+-- Its always 0 otherwise
 overflow <= interResult(31) when (operand1(31) = '0' and operand2(31) = '0') else
             complementedAdd(31) when (operand1(31) = '1' and operand2(31) = '1') else
 				'0';
 result <= interResult;
 			   
-		
---UADDR1: port map ('0'& operand1(30 downto 0),operand)
 end Behavioral;
 
