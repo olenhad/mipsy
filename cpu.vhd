21689dda7644726a03247b987ab3666ed9f3d016
unknown <hunur.khanna@gmail.com>
2013-11-13 17:36:10 +0800

    BNE included, cpu_op2 check removed 101% 98%

--- cpu.vhd
+++ cpu.vhd
@@ -264,9 +264,9 @@ begin
 			RAM1(0) <= prev_op1(15 downto 8);
 			RAM2(0) <= prev_op1(23 downto 16);
 			RAM3(0) <= prev_op1(31 downto 24);
-		end if;
-		
-		if(prev_op2 /= cpu_op2) then
+		--end if;
+		else
+		--if(prev_op2 /= cpu_op2) then
 			prev_op2 := cpu_op2;
 			RAM0(1) <= prev_op2(7 downto 0);
 			RAM1(1) <= prev_op2(15 downto 8);
