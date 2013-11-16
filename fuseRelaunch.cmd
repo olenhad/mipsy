d6472f411857901251efe8924a8edbfe06ae3420
Omer <momeriqbal2@gmail.com>
2013-11-16 18:59:19 +0800

    removed uneccessary files

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/Users/Hunar Khanna/Desktop/CG3207/VHDL/lab2/cg3207-project/test_cpu_isim_beh.exe" -prj "C:/Users/Hunar Khanna/Desktop/CG3207/VHDL/lab2/cg3207-project/test_cpu_beh.prj" "work.test_cpu" 
